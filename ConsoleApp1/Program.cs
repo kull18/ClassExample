38083f2dbe0a23b103fbb3e450b2356f5a6671a0
Regiber18 <regiber.montoya2005@hotmail.com>
2024-08-07 22:29:19 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿var number1 = new MyList<int>(5); 
+var nnumbers = new MyList<String>(20);
+
+//new number for my class
+var newNumbers = new MyList<String>(20);
+
+number1.Add(2);
+number1.Add(2);
+number1.Add(5); 
+number1.Add(5);
+number1.Add(20);
+
+Console.WriteLine(number1.getCount()); 
+public class MyList<T>
+{
+    private List<T> _list;
+    private int _limit; 
+
+    public MyList(int limit)
+    {
+        _limit = limit; 
+        _list = new List<T>(); 
+    }
+
+    public void Add(T Element) 
+    {
+        if(_list.Count < _limit)
+        {
+            _list.Add(Element);
+        }
+    }
+
+    public string getCount()
+    {
+        String content = "";
+        foreach (T Element in _list)
+        {
+            content += Element + ","; 
+        }
+
+        return content; 
+    }
+
+}
